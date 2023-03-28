532c31aefd1f498b0e464a799ccff6351242436c
josemlegal <josemlegal97@gmail.com>
2023-03-28 11:02:37 -0400

    Add the create tx method

--- src/routes/transactions/+page.server.ts
+++ src/routes/transactions/+page.server.ts
@@ -1,5 +1,6 @@
 import { fail, type Actions, type ServerLoad } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma';
+import type { Prisma } from '@prisma/client';
 
 export const load: ServerLoad = async () => {
 	return {
@@ -16,21 +17,25 @@ export const load: ServerLoad = async () => {
 
 export const actions: Actions = {
 	create: async ({ request }) => {
-		const { title, description } = Object.fromEntries(await request.formData()) as {
+		const { title, description, amount } = Object.fromEntries(
+			await request.formData()
+		) as unknown as {
 			title: string;
 			description: string;
+			amount: number;
 		};
 
 		try {
-			// await prisma.transaction.create({
-			// 	data: {
-			// 		title,
-			// 		description
-			// 	}
-			// });
+			await prisma.transaction.create({
+				data: {
+					title,
+					description,
+					amount
+				} as unknown as Prisma.TransactionCreateInput
+			});
 		} catch (err) {
 			console.error(err);
-			return fail(500, { message: 'Could not create the project.' });
+			return fail(500, { message: 'Could not create the trans.' });
 		}
 
 		return {
